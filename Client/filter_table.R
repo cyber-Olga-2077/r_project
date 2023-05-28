c181e274e192039f5567dce04fda69f21ab77167
cyber_Olga_2077 <krzyczkowskaolga@gmail.com>
2023-05-28 16:24:28 +0200

    Added ignoring invalid lines in data frames

--- Client/filter_table.R
+++ Client/filter_table.R
@@ -14,12 +14,15 @@ Start_year <- sapply(strsplit(Years_range, split = "-"), `[`, 1)
 End_year <- sapply(strsplit(Years_range, split = "-"), `[`, 2)
 Whole_range_years <- seq(Start_year, End_year, 1)
 
-Stations <- read.csv("Data/stations.csv", sep = "")
+stations <- read.csv("Data/stations.csv", sep = "")
 
-base_data <- read.csv("Data/1931.csv.gz", header = FALSE)
+base_data <- read.csv("Data/raw/1931.csv.gz", header = FALSE)
 data <- filter(base_data, is.element(V3, c("TMIN", "TMAX")))
 data <- select(data, V1, V2, V3, V4)
 
 data <- left_join(x = data, y = stations, by = join_by(V1 == ID))
 data <- select(data, -BI, -Name)
 colnames(data) <- c("WeatherStationID", "Date", "MeasurementType", "MeasurementValue", "Latitude", "Longitude", "Elevation", "WeatherStationName")
+data <- filter(data, WeatherStationName != "")
+
+write.csv(data, file=gzfile("Data/processed/1931.csv.gz"))
